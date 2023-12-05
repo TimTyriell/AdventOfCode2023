32e4ece658f706780eae395ed4cdd8bdecf8fa80
Tyryell <IceShield@web.de>
2023-12-05 22:30:50 +0100

    basejs/day2/002 implement part 2

--- baseJs-advent-of-code/day2/index.js
+++ baseJs-advent-of-code/day2/index.js
@@ -9,20 +9,20 @@ function partOne(filename) {
     const inputArray = fs.readFileSync(filename, { encoding: 'utf-8' }).trim().split('\r\n');
     const gameMap = new Map();
     const correctGames = [];
-    let iterator = 0;
     for (const line of inputArray) {
         const [game, entries] = line.split(': ');
         const gameEntries = entries.split('; ').map(entry => entry.split(', '));
         gameMap.set(game, gameEntries);
         let isValid = true;
         for (const handfullCubes of gameEntries) {
+            console.log(handfullCubes);
             for (const entry of handfullCubes) {
-
             const [count, color] = entry.split(' ');
             if (Number(count) >  maxCubes[color]) {
                 isValid = false;
                 break;
             }
+
         }
     }
     if (isValid) {
@@ -42,4 +42,47 @@ function sumIdOfGames(games) {
     return sum;
 }
 
-console.log(partOne('./input.txt'));
+function partTwo(filename) {
+    const inputArray = fs.readFileSync(filename, { encoding: 'utf-8' }).trim().split('\r\n');
+    const gameMap = new Map();
+    let cubePower = 0;
+    for (const line of inputArray) {
+        const [game, entries] = line.split(': ');
+        const gameEntries = entries.split('; ').map(entry => entry.split(', '));
+        gameMap.set(game, gameEntries);
+        let minCubes = {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        }
+        for (const handfullCubes of gameEntries) {
+        
+            for (const entry of handfullCubes) {
+                const [count, color] = entry.split(' ');
+                switch (color) {
+                    case "red":
+                        if (Number(count) > minCubes.red) {
+                            minCubes.red = Number(count);
+                        } 
+                        break;
+                    case "green":
+                        if (Number(count) > minCubes.green) {
+                            minCubes.green = Number(count);
+                        }
+                        break;
+                    case "blue": 
+                        if (Number(count) > minCubes.blue) {
+                            minCubes.blue = Number(count);
+                        }   
+                        break;   
+                }
+            }
+        }
+       
+            cubePower += minCubes.red * minCubes.green * minCubes.blue
+    }
+    
+    return cubePower;
+}
+
+console.log(partTwo('./input.txt'));
