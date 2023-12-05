bdaaed395ef840aa22ea352971d9b9cec1ea2a5a
Tyryell <IceShield@web.de>
2023-12-05 16:55:26 +0100

    basejs/day01/002

--- baseJs-advent-of-code/day1/index.js
+++ baseJs-advent-of-code/day1/index.js
@@ -1,9 +1,28 @@
 import fs from 'fs';
+const wordToNumberMap = {
+    'one': 'on1ne',
+    'two': 'tw2wo',
+    'three': 'thre3hree',
+    'four': 'fou4our',
+    'five': 'fiv5ive',
+    'six': 'si6ix',
+    'seven': 'sev7ven',
+    'eight': 'eigh8ight',
+    'nine': 'nin9ine'
+}
 
 function partOne(filenname) {
-    const lines = fs.readFileSync(filenname, {encoding: 'utf-8'}).trim().split('\r\n');
+    const lines = fs.readFileSync(filenname, { encoding: 'utf-8' }).trim().split('\r\n');
     console.log(lines);
-    const numbers = lines.map((line) => { 
+    const lines2 = lines.map((line) => {
+        for (let word in wordToNumberMap) {
+            let regex = new RegExp(word, 'gi');
+            line = line.replace(regex, wordToNumberMap[word]);
+        }
+        return line;
+    });
+    console.log(lines2);
+    const numbers = lines2.map((line) => {
         let firstNumber = line.split('').find((number) => Number.isInteger(Number(number)));
         let lastNumber = line.split('').findLast((number) => Number.isInteger(Number(number)));
         return Number(firstNumber + lastNumber);
