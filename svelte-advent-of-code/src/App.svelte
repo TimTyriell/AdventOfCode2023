e5bf25b0db4a93d360cc6aef7fc16a29a904cdee
Tyryell <IceShield@web.de>
2023-12-05 15:57:08 +0100

    svelte/day01/001

---
+++ svelte-advent-of-code/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Day1 from './lib/Day1.svelte'
+</script>
+
+<main>
+  <h5>Advent of code + Svelte</h5>
+  <div>
+    <a href="https://adventofcode.com/" target="_blank" rel="noreferrer">
+      <img src="https://repository-images.githubusercontent.com/112706767/160be980-3b1a-11eb-9dbe-439a40adfa99" class="logo" alt="Advent of Code Img" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  
+
+  <div class="card">
+    <Day1 />
+  </div>
+
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ svelte-advent-of-code/src/lib/Day1.svelte
@@ -0,0 +1,48 @@
+<script>
+  let inputText = "";
+  let formattedArray;
+  let inputArray;
+  let overAllNumber = 0;
+
+  async function loadInputData() {
+    const response = await fetch("inputDay1.txt");
+    inputText = await response.text();
+    inputArray = inputText.split("\n");
+  }
+
+  function solveProblem() {
+    overAllNumber = 0;
+    inputArray.forEach((element) => {
+      let firstNumber = 0;
+      let lastNumber;
+      for (let i = 0; i < element.length; i++) {
+        if (!isNaN(element[i])) {
+          if (firstNumber == 0) {
+            firstNumber = element[i];
+          }
+          lastNumber = element[i];
+        }
+      }
+      overAllNumber += parseInt(String(firstNumber) + String(lastNumber), 10);
+    });
+  }
+
+  // Load input data when the component is mounted
+  import { onMount } from "svelte";
+  onMount(async () => {
+    await loadInputData();
+    solveProblem();
+  });
+</script>
+
+<div>
+  <h1>Advent of Code Challenge</h1>
+
+  {#if inputText}
+    <h2>Input Data</h2>
+    <pre>{inputArray}</pre>
+    <pre>{overAllNumber}</pre>
+  {/if}
+
+  <!-- Implement the rest of your component as needed -->
+</div>
